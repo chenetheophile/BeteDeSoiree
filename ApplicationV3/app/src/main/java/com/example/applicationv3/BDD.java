f37b2325e34dbd1c5f0a785cd445cb24940a8cfe
chenetheophile <73079835+chenetheophile@users.noreply.github.com>
2021-03-29 15:00:56 +0200

    theo ajout bdd class 29/03/21

---
+++ ApplicationV3/app/src/main/java/com/example/applicationv3/BDD.java
@@ -0,0 +1,37 @@
+package com.example.applicationv3;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BDD {
+    public BDD(){
+
+    }
+    public FirebaseFirestore getBDD(){
+        return FirebaseFirestore.getInstance();//récupere la BDD
+    }
+    public void creerChamp(FirebaseFirestore base, String nomCollection, String nomDocument, ArrayList<String> nomChamp, ArrayList<String> valeur) throws Exception {
+        if(nomChamp.size()!=valeur.size()){
+            throw new Exception("Difference du nombre d'element entre nomChamp et Valeur");
+        }
+        else{
+            Map<String, Object> map = new HashMap<>();
+            for(int i=0;i<nomChamp.size();i++){
+                map.put(nomChamp.get(i),valeur.get(i));
+            }
+            base.collection(nomCollection).document(nomDocument)
+                    .set(map)
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("BDD", "Error adding document", e);
+                        }
+                    });
+        }
+    }
+}
