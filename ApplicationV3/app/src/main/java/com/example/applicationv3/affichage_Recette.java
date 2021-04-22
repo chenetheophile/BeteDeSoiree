f3156489086bf09eb7b704971fe0f2ed0354f23b
chenetheophile <73079835+chenetheophile@users.noreply.github.com>
2021-04-22 21:06:32 +0200

    theo ajout favori et retrait fav 22/04/21

--- ApplicationV3/app/src/main/java/com/example/applicationv3/affichage_Recette.java
+++ ApplicationV3/app/src/main/java/com/example/applicationv3/affichage_Recette.java
@@ -2,25 +2,18 @@ package com.example.applicationv3;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageView;
-import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.squareup.picasso.Picasso;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -33,10 +26,6 @@ public class affichage_Recette extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_affichage_recette);
 
-        try {
-            verifier();
-        } catch (FileNotFoundException e) {
-        }
         TextView txt=findViewById(R.id.listeIngredient);
         txt.setText(getIntent().getExtras().getString("Ingredient"));
 
@@ -44,11 +33,16 @@ public class affichage_Recette extends AppCompatActivity {
         Recette.setText(getIntent().getExtras().getString("Nom"));
 
         CheckBox fav=findViewById(R.id.fav);
+        if(verifier(Recette.getText().toString())) {
+            fav.setChecked(true);
+        }else{
+            fav.setChecked(false);
+        }
         fav.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(fav.isChecked()){
-                    sauver((String) Recette.getText());
+                    sauver((String) Recette.getText(),true,true);
                 }else{
                     supprimer((String) Recette.getText());
                 }
@@ -57,38 +51,66 @@ public class affichage_Recette extends AppCompatActivity {
         ImageView imgRecette=findViewById(R.id.img_recette);
         Picasso.get().load(getIntent().getExtras().getString("lien")).into(imgRecette);
     }
-    private void verifier() throws FileNotFoundException {
+    private boolean verifier(String recette) {
+        boolean verif=false;
+
         FileInputStream fis = null;
-        fis = affichage_Recette.this.openFileInput("Fav");
-        InputStreamReader inputStreamReader =
-                new InputStreamReader(fis, StandardCharsets.UTF_8);
-        StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(inputStreamReader)) {
+        try {
+            fis = affichage_Recette.this.openFileInput("Fav");
+            InputStreamReader inputStreamReader = new InputStreamReader(fis, StandardCharsets.UTF_8);
+            BufferedReader reader = new BufferedReader(inputStreamReader);
             String line = reader.readLine();
             while (line != null) {
-                stringBuilder.append(line).append('\n');
+                if (line.equalsIgnoreCase(recette)) {
+                    verif = true;
+                }
                 line = reader.readLine();
             }
         } catch (IOException e) {
-            // Error occurred when opening raw file for reading.
-        } finally {
-            String contents = stringBuilder.toString();
-            Log.i("Fav",contents);
+            sauver("",false,false);
         }
+        return verif;
     }
-    private void sauver(String recette){
+    private void sauver(String recette,boolean ajout,boolean mess){
+        if(mess){
         Toast.makeText(getApplicationContext(),recette+" Sauvegardé",Toast.LENGTH_SHORT).show();
-        String filename = "Fav";
+        }
         try {
-            try (FileOutputStream fos = getApplicationContext().openFileOutput(filename, Context.MODE_PRIVATE)) {
-                fos.write(recette.getBytes());
+            FileOutputStream outputStream;
+            recette+='\n';
+            String filename="Fav";
+            if (ajout) {
+                outputStream = openFileOutput(filename, Context.MODE_APPEND);
+            } else {
+                outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
             }
+            outputStream.write(recette.getBytes());
+            outputStream.flush();
+            outputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-    private void supprimer(String recette){
+        private void supprimer(String recette){
         Toast.makeText(getApplicationContext(),recette+" retiré des favoris",Toast.LENGTH_SHORT).show();
+        String liste="\n";
+        if(verifier(recette)){
+            FileInputStream fis = null;
+            try {
+                fis = affichage_Recette.this.openFileInput("Fav");
+                InputStreamReader inputStreamReader = new InputStreamReader(fis, StandardCharsets.UTF_8);
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String line = reader.readLine();
+                while (line != null) {
+                    if(!line.equalsIgnoreCase(recette)){
+                        liste=liste+line+'\n';
+                    }
+                    line = reader.readLine();
+                }
+                sauver(liste,false,false);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
