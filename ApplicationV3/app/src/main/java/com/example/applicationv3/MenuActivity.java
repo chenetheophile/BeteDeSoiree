68354f66472e9fcb07009c21c23aea0d49c521ec
chenetheophile <theophile.chene@esme.fr>
2021-05-13 14:50:13 +0200

    debut Recherche

--- ApplicationV3/app/src/main/java/com/example/applicationv3/MenuActivity.java
+++ ApplicationV3/app/src/main/java/com/example/applicationv3/MenuActivity.java
@@ -1,6 +1,12 @@
 package com.example.applicationv3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -12,15 +18,25 @@ import com.example.applicationv3.ui.main.SectionsPagerAdapter;
 import java.util.ArrayList;
 
 public class MenuActivity extends AppCompatActivity {
-
+    private TabLayout tabs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ImageButton boutonRech=findViewById(R.id.boutonrecherche);
+        boutonRech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent rech=new Intent(getApplicationContext(),Recherche.class);
+                rech.putExtra("Tout",getIntent().getExtras());
+                startActivity(rech);
+            }
+        });
+
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
+        tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
     }
 }
