effe12d69998de8e5b310e9b56489dea1f821a22
chenetheophile <theophile.chene@esme.fr>
2021-05-14 19:47:41 +0200

    Test resoud fb

--- ApplicationV3/app/src/main/java/com/example/applicationv3/CocktailsFragment.java
+++ ApplicationV3/app/src/main/java/com/example/applicationv3/CocktailsFragment.java
@@ -4,29 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.view.menu.ActionMenuItemView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseUser;
+
 
 import java.util.ArrayList;
 
