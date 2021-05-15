2f9cc96d44daff4739034bf7b5c362a178e71f14
chenetheophile <theophile.chene@esme.fr>
2021-05-15 16:41:55 +0200

    rechercher complete+progress bar 15/05

--- ApplicationV3/app/src/main/java/com/example/applicationv3/JeuxAdapter.java
+++ ApplicationV3/app/src/main/java/com/example/applicationv3/JeuxAdapter.java
@@ -17,24 +17,22 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class JeuxAdapter extends RecyclerView.Adapter<JeuxAdapter.JeuxHolder> implements Filterable {
+public class JeuxAdapter extends RecyclerView.Adapter<JeuxAdapter.JeuxHolder> implements Filterable{
 
     LayoutInflater mInflater;
     private ArrayList<String> noms_jeux=new ArrayList<>();
     private ArrayList<String> joueurs_jeux=new ArrayList<>();
     private ArrayList<String> equipement_jeux=new ArrayList<>();
     private ArrayList<Integer> images_jeux=new ArrayList<>();
+    private ArrayList<Item>recipe=new ArrayList<>();
     private ArrayList<Item>listepleine=new ArrayList<>();
-    private ArrayList<Item>listerecette=new ArrayList<>();
     private Context context;
 
-    public void recetteAdapter(ArrayList<Item> liste){
-        this.listerecette=liste;
-        listepleine=new ArrayList<>(liste);
-    }
+
     public JeuxAdapter(Context c, ArrayList<Item>recette , ArrayList<Integer> img){
         this.context=c;
-        listepleine.addAll(recette);
+        this.recipe=recette;
+        listepleine=new ArrayList<>(recette);
         for(int i=0;i<recette.size();i++){
             noms_jeux.add(recette.get(i).getNom());
             joueurs_jeux.add(recette.get(i).getIngredient());
@@ -47,45 +45,54 @@ public class JeuxAdapter extends RecyclerView.Adapter<JeuxAdapter.JeuxHolder> im
     @NonNull
     @Override
     public JeuxHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-       return new JeuxHolder(mInflater.inflate(R.layout.listview_jeux, null));
+        return new JeuxHolder(mInflater.inflate(R.layout.listview_jeux, null),viewType);
 
     }
 
     @Override
     public void onBindViewHolder(@NonNull JeuxHolder holder, int position) {
-        String nom = noms_jeux.get(position);
-        String detail = joueurs_jeux.get(position);
-        String desc = equipement_jeux.get(position);
-        holder.NomJeuTextView.setText(nom);
-        holder.DetailJeuTextView.setText(detail);
-        holder.DescJeuTextView.setText(desc);
-        holder.ObjetImageView.setImageResource(images_jeux.get(position));
-        holder.layout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent;
-                switch (listepleine.get(position).getType()){
-                    case "Cocktail":
-                    case "Recette":
-                        intent=new Intent(context,affichage_Recette.class);
-                        intent.putExtra("Recette",listepleine.get(position));
-                        break;
-                    case "Jeux":
-                        intent=new Intent(context,Joueurs.class);
-                        intent.putExtra("IdJeu",position);
-                        break;
-                    case "Musique":
-                        intent=new Intent(context,testSpot.class);
-                        break;
-                    default:
-                        intent=null;
-                        break;
 
+        if (recipe.size()==position-1 ||recipe.size()<=position){
+            Log.i("search","suppr");
+            holder.layout.setVisibility(View.GONE);
+        }else{
+            Item item=recipe.get(position);
+            String nom = item.getNom();
+            String detail = item.getIngredient();
+            String desc = item.getDescription();
+            holder.layout.setVisibility(View.VISIBLE);
+            holder.NomJeuTextView.setText(nom);
+            holder.DetailJeuTextView.setText(detail);
+            holder.DescJeuTextView.setText(desc);
+            holder.ObjetImageView.setImageResource(images_jeux.get(position));
+            holder.layout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent;
+                    switch (recipe.get(position).getType()){
+                        case "Cocktail":
+                        case "Recette":
+                            intent=new Intent(context,affichage_Recette.class);
+                            intent.putExtra("Recette",recipe.get(position));
+                            break;
+                        case "Jeux":
+                            intent=new Intent(context,Joueurs.class);
+                            intent.putExtra("IdJeu",position);
+                            break;
+                        case "Musique":
+                            intent=new Intent(context,testSpot.class);
+                            break;
+                        default:
+                            intent=null;
+                            break;
+
+                    }
+                    context.startActivity(intent);
                 }
-                context.startActivity(intent);
-            }
-        });
+            });
+
+        }
+
 
     }
 
@@ -102,6 +109,25 @@ public class JeuxAdapter extends RecyclerView.Adapter<JeuxAdapter.JeuxHolder> im
     @Override
     public long getItemId(int position) {
         return position;
+    }
+
+
+    public class JeuxHolder extends RecyclerView.ViewHolder{
+        TextView NomJeuTextView;
+        TextView DetailJeuTextView;
+        TextView DescJeuTextView ;
+        ImageView ObjetImageView ;
+        CardView layout;
+
+
+        public JeuxHolder(@NonNull View itemView,int viewType) {
+            super(itemView);
+            NomJeuTextView = (TextView) itemView.findViewById(R.id.NomJeuTextView);
+            DetailJeuTextView = (TextView) itemView.findViewById(R.id.DetailJeuTextView);
+            DescJeuTextView = (TextView) itemView.findViewById(R.id.DescJeuTextView);
+            ObjetImageView = (ImageView) itemView.findViewById(R.id.ObjetImageView);
+            layout=itemView.findViewById(R.id.boiteItem);
+        }
     }
 
     @Override
@@ -130,25 +156,21 @@ public class JeuxAdapter extends RecyclerView.Adapter<JeuxAdapter.JeuxHolder> im
 
         @Override
         protected void publishResults(CharSequence constraint, FilterResults results) {
-            listerecette.clear();
-            listerecette.addAll((ArrayList)results.values);
+            recipe.clear();
+            recipe.addAll((ArrayList)results.values);
+            for (int i=0;i<recipe.size();i++){
+                Log.i("searchRECIPE",toString(recipe.get(i)));
+            }
             notifyDataSetChanged();
+
         }
+
+        private String toString(Item item) {
+            String chaine = "";
+            chaine += item.getNom();
+            return chaine;
+        }
+
     };
-    public class JeuxHolder extends RecyclerView.ViewHolder{
-        TextView NomJeuTextView;
-        TextView DetailJeuTextView;
-        TextView DescJeuTextView ;
-        ImageView ObjetImageView ;
-        CardView layout;
-        public JeuxHolder(@NonNull View itemView) {
-            super(itemView);
-            NomJeuTextView = (TextView) itemView.findViewById(R.id.NomJeuTextView);
-            DetailJeuTextView = (TextView) itemView.findViewById(R.id.DetailJeuTextView);
-            DescJeuTextView = (TextView) itemView.findViewById(R.id.DescJeuTextView);
-            ObjetImageView = (ImageView) itemView.findViewById(R.id.ObjetImageView);
-            layout=itemView.findViewById(R.id.boiteItem);
-        }
-    }
 
 }
